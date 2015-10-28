58f7fd3e2a1714665cdad5c0c2de7bc54954f3a7
Folgert Karsdorp <fbkarsdorp@gmail.com>
2015-10-28 14:11:41 +0100

    update function arguments

--- textnet/bootstrap_network.py
+++ textnet/bootstrap_network.py
@@ -1,5 +1,4 @@
 from collections import defaultdict, Counter
-from functools import partial
 
 import pyprind
 
@@ -10,60 +9,12 @@ import pandas as pd
 from sklearn.metrics import pairwise_distances
 
 
-def time_grouped_pairwise_distances(X, time_index, metric='cosine', groupby=5, n_jobs=1):
-    """
-    This is a specialized function to compute the distances between texts and potential
-    pre-texts where pre-texts are defined as those texts that are published (or attested)
-    prior to or at the same time that the source text was published. The function is 
-    basically a little trick that reduced the costly operation of computing all pairwise
-    distances in a matrix of stories where we know in advance that certain texts cannot
-    be each others nearest neighbor based on their date of appearance. 
-
-    Parameters
-    ----------
-    X : ndarray or sparse array, shape: (n_samples_X, n_features)
-        Input data.
-    time_index : ndarray or pandas DatetimeIndex, shape: (n_samples_X), 
-        Index corresponding to time points of each sample in X. If supplied,
-        neighbors for each item x in X will only consist of samples that occur 
-        before or at the time point corresponding with x. Default is None.
-    metric : string, or callable
-        Valid values for metric are:
-            - From scikit-learn: ['cityblock', 'cosine', 'euclidean', 'l1', 'l2',
-              'manhattan']. These metrics support sparse matrix inputs.
-            - From scipy.spatial.distance: ['braycurtis', 'canberra', 'chebyshev',
-              'correlation', 'dice', 'hamming', 'jaccard', 'kulsinski', 'mahalanobis',
-              'matching', 'minkowski', 'rogerstanimoto', 'russellrao', 'seuclidean',
-              'sokalmichener', 'sokalsneath', 'sqeuclidean', 'yule']
-        See the documentation for scipy.spatial.distance for details on these
-        metrics. These metrics do not support sparse matrix inputs.
-    groupby : integer, default 5
-        group the input array X by chunks of n years where n is specified by groupby.
-    n_jobs : integer, default 1
-        The number of jobs to use for the computation. This works by breaking
-        down the pairwise matrix into n_jobs even slices and computing them in
-        parallel.          
-    """
-    if not isinstance(time_index, pd.DatetimeIndex):
-        time_index = pd.DatetimeIndex(time_index)
-    dm = np.zeros((X.shape[0], X.shape[0]))
-    grouped_indices = time_index.year // groupby * groupby
-    for year in np.unique(grouped_indices):
-        Y_indices = grouped_indices <= year
-        X_indices = grouped_indices == year
-        if X_indices.sum() > 0 and Y_indices.sum() > 0:
-            chunk_dm = pairwise_distances(X[X_indices,: ], Y=X[Y_indices,: ], metric=metric, n_jobs=n_jobs)
-            dm[np.ix_(X_indices, Y_indices)] = chunk_dm
-    return dm
-
-
 def all_argmin(array, tol=0.001):
     return np.where(np.abs(array - np.array([np.nanmin(array, axis=1)]).T) < tol)
 
 
 def bootstrap_neighbors(X, time_index=None, sigma=0.5, sample_prop=0.5, 
-                        n_iter=1000, metric="cosine", n_jobs=1, all_min=False, 
-                        grouped_pairwise=False, groupby=5):
+                        n_iter=1000, metric="cosine", n_jobs=1, all_min=False):
     """
     Parameters
     ----------
@@ -94,29 +45,18 @@ def bootstrap_neighbors(X, time_index=None, sigma=0.5, sample_prop=0.5,
         The number of jobs to use for the computation. This works by breaking
         down the pairwise matrix into n_jobs even slices and computing them in
         parallel.
-    grouped_pairwise : boolean, default False
-        Set to True if you want to use the time_grouped_pairwise_distances, 
-        False, otherwise. This option is especially useful for large arrays.
-    groupby : integer, default 5
-        group the input array X by chunks of n years where n is specified by groupby.
-        only works in combination with grouped_pairwise
     """
     n_samples, n_features = X.shape
     sample_size = int(n_features * sample_prop)
     neighbors = np.zeros((n_samples, n_samples), dtype=np.float64)
     indices = np.arange(n_samples)
     progress = pyprind.ProgBar(n_iter)
-    if grouped_pairwise:
-        dist_fn = partial(time_grouped_pairwise_distances, time_index=time_index, 
-                          metric=metric, n_jobs=n_jobs, groupby=groupby)
-    else:
-        dist_fn = partial(pairwise_distances, metric=metric, n_jobs=n_jobs)
     if time_index is not None:
         potential_neighbors = time_index <= time_index[np.newaxis].T        
     for iteration in range(n_iter):
         rnd_features = np.random.randint(n_features, size=sample_size)
         _X = X[:, rnd_features]
-        dm = dist_fn(_X)
+        dm = pairwise_distances(_X, metric=metric, n_jobs=n_jobs)
         np.fill_diagonal(dm, np.inf)
         if time_index is not None:
             dm[~potential_neighbors] = np.nan # no fix yet for problem of first text
@@ -129,7 +69,8 @@ def bootstrap_neighbors(X, time_index=None, sigma=0.5, sample_prop=0.5,
     return neighbors
 
 
-def bootstrap_neighbors_sparse_batch(X, time_index=None, n_iter=1000, sample_prop=0.5, metric="cosine", n_jobs=1):
+def bootstrap_neighbors_sparse_batch(X, time_index=None, n_iter=1000, sample_prop=0.5, 
+                                     metric="cosine", n_jobs=1, time_step=1):
     if not isinstance(time_index, pd.DatetimeIndex):
         time_index = pd.DatetimeIndex(time_index)
 
@@ -137,7 +78,7 @@ def bootstrap_neighbors_sparse_batch(X, time_index=None, n_iter=1000, sample_pro
     sample_size = int(n_features * sample_prop)
     neighbors = defaultdict(Counter)
 
-    grouped_indices = time_index.year // 5 * 5
+    grouped_indices = time_index.year // time_step * time_step
     progress = pyprind.ProgBar(n_iter)
 
     for iteration in range(n_iter):
