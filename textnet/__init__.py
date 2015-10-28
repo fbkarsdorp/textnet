170259b79e5238c74d1e4dc242253d3de22fcf57
Folgert Karsdorp <fbkarsdorp@gmail.com>
2015-10-28 14:13:07 +0100

    add import

--- textnet/__init__.py
+++ textnet/__init__.py
@@ -1,2 +1,2 @@
-from .bootstrap_network import bootstrap_neighbors, bootstrap_network, to_graph
+from .bootstrap_network import bootstrap_neighbors, bootstrap_neighbors_sparse_batch, bootstrap_network, to_graph
 from .statistics import graph_statistics, evolving_graph_statistics
