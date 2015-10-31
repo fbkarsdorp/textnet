eb241663ff5ea365287b3aba3cd18c1e1f80f9d0
Folgert Karsdorp <fbkarsdorp@gmail.com>
2015-10-31 15:29:22 +0100

    add temporal scatterplot

--- textnet/__init__.py
+++ textnet/__init__.py
@@ -1,3 +1,4 @@
 from .bootstrap_network import bootstrap_neighbors, bootstrap_neighbors_sparse_batch
 from .bootstrap_network import bootstrap_network, to_graph, evolving_graphs
 from .statistics import graph_statistics, evolving_graph_statistics
+from .visuals import timeline_scatter_plot
---
+++ textnet/visuals.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import seaborn as sns
+sns.set_style("white")
+
+from sklearn.manifold import MDS, TSNE
+from sklearn.metrics import pairwise_distances
+
+
+def timeline_scatter_plot(X, time_index, method='MDS', metric='cosine', **kwargs):
+    if not isinstance(time_index, pd.DatetimeIndex):
+        time_index = pd.DatetimeIndex(time_index)
+    dm = pairwise_distances(X, metric=metric)
+    if method.upper() == 'MDS':
+        decomposer = MDS(n_components=2, dissimilarity='precomputed', verbose=1, **kwargs)
+        decomposer.fit(dm)
+    elif method.upper() == 'TSNE':
+        decomposer = TSNE(n_components=2, metric='precomputed', verbose=1, **kwargs)
+        decomposer.fit(dm)
+    else:
+        raise ValueError("Method %s is not supported..." % method)
+    X, Y = decomposer.embedding_[:,0], decomposer.embedding_[:,1]
+    unique_index = time_index.unique().order()
+    colormap = {time_stamp: color for time_stamp, color in zip(unique_index, sns.cubehelix_palette(unique_index.shape[0]))}
+    colors = [colormap[time_stamp] for time_stamp in time_index]
+    sns.plt.scatter(X, Y, s=40, color=colors, alpha=0.7)
+    sns.plt.axis('off')
+
+
