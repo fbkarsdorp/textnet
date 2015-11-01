30147d34309f1ea3c9cc92a793776ab15e0be28f
Folgert Karsdorp <fbkarsdorp@gmail.com>
2015-11-01 09:23:32 +0100

    add functions to conveniently extract end state of random graphs

--- textnet/random.py
+++ textnet/random.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import random
 
+from collections import deque
+
 import networkx as nx
 import numpy as np
 import pandas as pd
@@ -9,7 +11,7 @@ from .statistics import evolving_graph_statistics
 from .utils import node_counter
 
 
-def randomized_dynamic_time_graph(neighbors, time_index, m=1, groupby=lambda x: x, sigma=0.5):
+def randomized_dynamic_time_graph(neighbors, time_index, m=1, groupby=lambda x: x):
     """
     Returns a generator of random graphs at each time step t in time_index 
     according to the Barabási–Albert preferential attachment model. 
@@ -17,7 +19,7 @@ def randomized_dynamic_time_graph(neighbors, time_index, m=1, groupby=lambda x: 
     Parameters
     ----------
     neighbors : output of textnet.bootstrap_neighbors or textnet.bootstrap_neighbors_sparse_batch
-    time_index : ndarray of Timestamps or pandas DatetimeIndex, shape: (n_nodes) 
+    time_index : numpy.ndarray of Timestamps or pandas.DatetimeIndex, shape: (n_nodes) 
         Index corresponding to time points of each sample in G. If supplied,
         neighbors for each node n in G will only consist of samples that occur 
         before or at the time point corresponding with x.
@@ -25,11 +27,8 @@ def randomized_dynamic_time_graph(neighbors, time_index, m=1, groupby=lambda x: 
         Number of edges to attach from a new node to existing nodes
     groupby : callable
         Function specifying the time steps at which the graphs should be created
-    sigma : float, default 0.5
-        The threshold percentage of how often a data point must be 
-        assigned as nearest neighbor.        
     """
-    stats = evolving_graph_statistics(neighbors, time_index, groupby=groupby, sigma=sigma).sort_index()
+    stats = evolving_graph_statistics(neighbors, time_index, groupby=groupby).sort_index()
     G = nx.DiGraph()
     # create an empty graph with the nodes at the first time step 
     G.add_nodes_from(range(stats.n[0]))
@@ -47,6 +46,26 @@ def randomized_dynamic_time_graph(neighbors, time_index, m=1, groupby=lambda x: 
         yield stats.index[i], G
 
 
+def randomized_time_graph(neighbors, time_index, m=1, groupby=lambda x: x):
+    """
+    Returns a random graphs based on the empirical data 
+    according to the Barabási–Albert preferential attachment model. 
+
+    Parameters
+    ----------
+    neighbors : output of textnet.bootstrap_neighbors or textnet.bootstrap_neighbors_sparse_batch
+    time_index : numpy.ndarray of Timestamps or pandas.DatetimeIndex, shape: (n_nodes) 
+        Index corresponding to time points of each sample in G. If supplied,
+        neighbors for each node n in G will only consist of samples that occur 
+        before or at the time point corresponding with x.
+    m : int, default 1
+        Number of edges to attach from a new node to existing nodes
+    groupby : callable
+        Function specifying the time steps at which the graphs should be created   
+    """ 
+    return deque(randomized_dynamic_time_graph(neighbors, time_index, m=m, groupby=groupby))[0][1]
+
+
 def gnp_random_dynamic_time_graph(neighbors, time_index, p, groupby=lambda x: x): 
     """
     Returns a generator of random graphs at each time step t according to the Erdős-Rényi
@@ -55,7 +74,7 @@ def gnp_random_dynamic_time_graph(neighbors, time_index, p, groupby=lambda x: x)
     Parameters
     ----------
     neighbors : output of textnet.bootstrap_neighbors or textnet.bootstrap_neighbors_sparse_batch
-    time_index : ndarray of Timestamps or pandas DatetimeIndex, shape: (n_nodes) 
+    time_index : numpy.ndarray of Timestamps or pandas.DatetimeIndex, shape: (n_nodes) 
         Index corresponding to time points of each sample in G. If supplied,
         neighbors for each node n in G will only consist of samples that occur 
         before or at the time point corresponding with x.
@@ -77,3 +96,22 @@ def gnp_random_dynamic_time_graph(neighbors, time_index, p, groupby=lambda x: x)
                     G.add_node(_nodes[neighbor], name=neighbor, date=time_index[neighbor])
                     G.add_edge(_nodes[story_id], _nodes[neighbor])
         yield group_id, G
+
+def gnp_random_time_graph(neighbors, time_index, p, groupby=lambda x: x):
+    """
+    Returns a random graph based on the empirical data according to the Erdős-Rényi
+    graph model. Possible edges are defined by the time steps and probability p.
+
+    Parameters
+    ----------
+    neighbors : output of textnet.bootstrap_neighbors or textnet.bootstrap_neighbors_sparse_batch
+    time_index : numpy.ndarray of Timestamps or pandas.DatetimeIndex, shape: (n_nodes) 
+        Index corresponding to time points of each sample in G. If supplied,
+        neighbors for each node n in G will only consist of samples that occur 
+        before or at the time point corresponding with x.
+    p : float
+        probability of creating an edge.
+    groupby : callable
+        Function specifying the time steps at which the graphs should be created
+    """
+    return deque(gnp_random_dynamic_time_graph(neighbors, time_index, p, groupby=groupby), maxlen=1)[0][1]
