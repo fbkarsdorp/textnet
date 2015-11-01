f89250c7e3c791eb4566e1a9bc7f7765891cc1cb
Folgert Karsdorp <fbkarsdorp@gmail.com>
2015-11-01 09:03:21 +0100

    add gnp random time graph

--- textnet/random.py
+++ textnet/random.py
@@ -1,32 +1,12 @@
+# -*- coding: utf-8 -*-
+import random
+
 import networkx as nx
 import numpy as np
+import pandas as pd
 
 from .statistics import evolving_graph_statistics
-from .utils import nx2igraph
-
-
-def rewire_dynamic_time_graph(G, time_index, rewire_prop=1.0):
-    """
-    This function rewires the edges in a time-indexed graph given a
-    rewire probability. This is the most naive randomization presented
-    here, because the time-dependent anchoring of nodes is lost.
-
-    Parameters
-    ----------
-    G : graph : igraph.Graph(directed=True) or nx.DiGraph
-        The input graph, can be constructed using textnet.to_graph or one of the
-        other graph creating functions.
-    time_index : ndarray of Timestamps or pandas DatetimeIndex, shape: (n_nodes) 
-        Index corresponding to time points of each sample in G. If supplied,
-        neighbors for each node n in G will only consist of samples that occur 
-        before or at the time point corresponding with x.
-    rewire_prop : float, default 1.0
-        rewiring probability
-    """
-    G = nx2igraph(G) if isinstance(G, (nx.Graph, nx.DiGraph)) else G.copy()
-    G.rewire_edges(rewire_prop)
-    for time_stamp in time_index.unique().order():
-        yield time_stamp, G.subgraph(G.vs.select(date_le=time_stamp))
+from .utils import node_counter
 
 
 def randomized_dynamic_time_graph(neighbors, time_index, m=1, groupby=lambda x: x, sigma=0.5):
@@ -67,4 +47,36 @@ def randomized_dynamic_time_graph(neighbors, time_index, m=1, groupby=lambda x: 
         yield stats.index[i], G
 
 
+def gnp_random_dynamic_time_graph(neighbors, time_index, p, groupby=lambda x: x, sigma=0.5): 
+    """
+    Returns a generator of random graphs at each time step t according to the Erdős-Rényi
+    graph model. Possible edges are defined by the time steps and probability p.
 
+    Parameters
+    ----------
+    neighbors : output of textnet.bootstrap_neighbors or textnet.bootstrap_neighbors_sparse_batch
+    time_index : ndarray of Timestamps or pandas DatetimeIndex, shape: (n_nodes) 
+        Index corresponding to time points of each sample in G. If supplied,
+        neighbors for each node n in G will only consist of samples that occur 
+        before or at the time point corresponding with x.
+    p : float
+        probability of creating an edge.
+    groupby : callable
+        Function specifying the time steps at which the graphs should be created
+    sigma : float, default 0.5
+        The threshold percentage of how often a data point must be 
+        assigned as nearest neighbor.     
+    """
+    index_series = pd.Series(sorted(neighbors.keys()), index=time_index)
+    G = nx.DiGraph()
+    potential_neighbors = time_index <= time_index[np.newaxis].T
+    _nodes = node_counter()
+    for group_id, story_ids in index_series.groupby(groupby):
+        for story_id in story_ids:
+            G.add_node(_nodes[story_id], name=story_id, date=time_index[story_id])
+            neighbors, _ = np.where(potential_neighbors[story_id])
+            for neighbor in neighbors:
+                if random.random() < p:
+                    G.add_node(_nodes[neighbor], name=neighbor, date=time_index[neighbor])
+                    G.add_edge(_nodes[story_id], _nodes[neighbor])
+        yield group_id, G
