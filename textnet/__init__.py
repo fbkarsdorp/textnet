d9e7a51f47b5f8bded04d06f1a114cb529ee1abe
Folgert Karsdorp <fbkarsdorp@gmail.com>
2015-11-06 12:29:03 +0000

    fix?

--- textnet/__init__.py
+++ textnet/__init__.py
@@ -1,6 +1,6 @@
 from .network import bootstrap_neighbors, bootstrap_neighbors_sparse_batch
 from .network import bootstrap_network, to_graph, evolving_graphs
-from .random import randomized_dynamic_time_graph, gnp_random_dynamic_time_graph, randomized_time_graph, gnp_random_time_graph
-from .random import rewire_dynamic_time_graph, rewired_time_graph
+from .random import randomized_dynamic_time_graph, gnp_random_dynamic_time_graph
+from .random import rewire_dynamic_time_graph
 from .statistics import graph_statistics, evolving_graph_statistics
 from .visuals import timeline_scatter_plot
